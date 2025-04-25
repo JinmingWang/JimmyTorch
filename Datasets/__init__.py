c7a06bc5bfa47b232a30cfd45f910122270516b6
JinmingWang <13106C1@gmail.com>
2025-04-25 01:28:09 +0100

    Add SequenceUtils

--- Datasets/__init__.py
+++ Datasets/__init__.py
@@ -6,5 +6,7 @@ from .DatasetUtils import DEVICE
 from .TrajectoryUtils import Traj, BatchTraj, getLat, getLng, computeDistance, cropPadTraj, flipTrajWestEast, \
     flipTrajNorthSouth, centerTraj, zScoreTraj, minMaxTraj, rotateTraj, interpTraj, plotTraj
 
+from .SequenceUtils import cropPadSequence
+
 from typing import *
 import torch
