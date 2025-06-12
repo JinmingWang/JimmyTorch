cd7b31e0df5328ee2eda5875f61221510922b4cd
JinmingWang <13106C1@gmail.com>
2025-06-12 13:22:17 +0100

    Add RMSE Loss Function

--- Models/__init__.py
+++ Models/__init__.py
@@ -6,7 +6,7 @@ from .Basics import *
 from .CostTest import testCudaSpeed, testFlops
 from .Attentions import SELayer1D, SELayer2D, MHSA
 from .ModelUtils import Transpose, Permute, Reshape, PrintShape, SequentialMultiIO, makeItResidual, Rearrange
-from .LossFunctions import MaskedLoss, SequentialLossWithLength
+from .LossFunctions import MaskedLoss, SequentialLossWithLength, RMSE
 from .Functional import extendAs
 from .JimmyModel import JimmyModel
 
